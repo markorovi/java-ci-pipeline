17943f4774a758c61607be74e75b3d78141c0f4a
Marco <markorovi24@gmail.com>
2021-03-25 20:22:20 -0600

    JUnit

---
+++ test/Scanner/CalculatorTest.java
@@ -0,0 +1,24 @@
+package Scanner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void addition() {
+    }
+
+    @Test
+    void substraction() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void division() {
+    }
+}
---
+++ test/Scanner/ScanTest.java
@@ -0,0 +1,12 @@
+package Scanner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScanTest {
+
+    @Test
+    void read() {
+    }
+}
